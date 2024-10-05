847b0c31e21f27b9173cd1eff80e264b121d6599
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-10-05 12:21:06 +0530

    add sidebar

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Providers } from "../provider";
-import AppBarClient from "./components/AppBarClient";
+import AppBarClient from "../components/AppBarClient";
 
 const inter = Inter({ subsets: ["latin"] });
 
